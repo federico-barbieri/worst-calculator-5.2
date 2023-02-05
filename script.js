c4af1a4ef5a071e199f79a8ee57c54bbeb77d409
Federico Barbieri <federicobarbieri54@gmail.com>
2023-02-05 17:17:19 +0100

    added logic to appending

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 // grab first number 
 
-let firstNumber = document.querySelector('#firstnumber').value;
+let firstNumber = document.querySelector('#firstnumber');
 
 // grab second number
 
-let secondNumber = document.querySelector('#secondnumber').value;
+let secondNumber = document.querySelector('#secondnumber');
 
 // grab operators
 const operatorSelector = document.querySelector('#operator');
@@ -22,20 +22,84 @@ const resultsList = document.querySelector('#results');
 // create a function for the 4: 
 //addition, substraction, multiplication and division
 
+
 function checkOperator(){
     if (operatorSelector.value == "add"){
-        return parseInt(firstNumber) + parseInt(secondNumber);
+
+       // store addition in a variable
+
+        let combination = parseInt(firstNumber.value) + parseInt(secondNumber.value); 
+
+        // create a new li, change its text content to the previous num
+        // and append it 
+
+        let newLi = document.createElement('li');
+        newLi.textContent = combination;
+        resultsList.appendChild(newLi);
+
+        // change the first value to the combination of 1 + 2
+
+        firstNumber.value = combination;
+        
     } else if (operatorSelector.value == "sub"){
-        return parseInt(firstNumber) - parseInt(secondNumber);
+
+               // store addition in a variable
+
+               let combination = parseInt(firstNumber.value) - parseInt(secondNumber.value); 
+
+               // create a new li, change its text content to the previous num
+               // and append it 
+       
+               let newLi = document.createElement('li');
+               newLi.textContent = combination;
+               resultsList.appendChild(newLi);
+       
+               // change the first value to the combination of 1 - 2
+       
+               firstNumber.value = combination;
+
+
     } else if (operatorSelector.value == "mul"){
-        return parseInt(firstNumber) * parseInt(secondNumber);
+
+         // store addition in a variable
+
+        let combination = parseInt(firstNumber.value) * parseInt(secondNumber.value); 
+
+         // create a new li, change its text content to the previous num
+         // and append it 
+            
+         let newLi = document.createElement('li');
+         newLi.textContent = combination;
+         resultsList.appendChild(newLi);
+            
+         // change the first value to the combination of 1 * 2
+            
+         firstNumber.value = combination;   
+
+
+
     } else if (operatorSelector.value == "div"){
-        return parseInt(firstNumber) / parseInt(secondNumber);
+
+         // store addition in a variable
+
+         let combination = parseInt(firstNumber.value) / parseInt(secondNumber.value); 
+
+         // create a new li, change its text content to the previous num
+         // and append it 
+            
+         let newLi = document.createElement('li');
+         newLi.textContent = combination;
+         resultsList.appendChild(newLi);
+            
+         // change the first value to the combination of 1 / 2
+            
+         firstNumber.value = combination;   
+
     }
 }
 
 calculateBtn.addEventListener('click', () =>{
-    let newLi = document.createElement('li');
-    newLi.innerHTML = checkOperator();
-    resultsList.appendChild(newLi);
+
+        checkOperator();
+
 })
